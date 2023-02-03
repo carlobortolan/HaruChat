4fbd3ccc60ae2e9488e2dfa4b71de70ec155f9a0
carlobortolan <carlobortolan@gmail.com>
2023-02-03 01:31:48 +0100

    Basic project setup & added dependencies

---
+++ src/main/java/com/example/haruchat/HaruChatApplication.java
@@ -0,0 +1,13 @@
+package com.example.haruchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaruChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaruChatApplication.class, args);
+    }
+
+}
